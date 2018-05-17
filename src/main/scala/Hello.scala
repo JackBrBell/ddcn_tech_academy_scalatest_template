aa0e472ebc55b3a5f1bbb6d1ad1e22f4e8d975a0
jackbrbell <jack.bell@digital.hmrc.gov.uk>
2018-05-17 12:55:44 +0100

    new commit

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,8 +1,5 @@
 object CrossOver extends App {
 
-  def chrom (chromo1:String, chromo2: String, index: Int): List[String]={
-    val a: List[String] = List("1","0")
-    a
+  def chrom (chromo1:String, chromo2: String, index: Int): List[String]= {
+    val a: List[String] = List("1", "0")
   }
-
-}
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -7,7 +7,9 @@ class CrossOverSpec extends WordSpec with MustMatchers {
     "Return 0 when given 1" in {
        CrossOver.chrom("0", "1", 0)  mustEqual List("1", "0")
     }
-
+"Return 000 when given 111" in {
+  CrossOver.chrom("000", "111", 1) mustEqual List("100", "011")
+}
   }
 
 }
